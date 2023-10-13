1102d1f74d70cce623431d7502e3a1c4d6bb5e5a
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 23:24:31 +0900

    Test on rails 7.1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,6 +8,8 @@ require "ii_interactor"
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults Rails::VERSION::STRING.to_f if config.respond_to?(:load_defaults)
+
     config.after_initialize do
       IIInteractor::LogSubscriber.attach_to(:ii_interactor)
     end
